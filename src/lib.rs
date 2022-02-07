77c9c952856a8c5e00d06d3b530c82fc1152ea9f
joshua enokson <kilograhm@pm.me>
2022-02-07 11:13:15 -0800

    added arc around uuid

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,12 @@
 pub use bytes::Bytes;
 use msg_store::Uuid;
+use std::sync::Arc;
 
 pub trait Db: Send + Sync {
-    fn get(&mut self, uuid: Uuid) -> Result<Bytes, String>;
-    fn add(&mut self, uuid: Uuid, msg: Bytes, msg_byte_size: u32) -> Result<(), String>;
-    fn del(&mut self, uuid: Uuid) -> Result<(), String>;
-    fn fetch(&mut self) -> Result<Vec<(Uuid, u32)>, String>;
+    fn get(&mut self, uuid: Arc<Uuid>) -> Result<Bytes, String>;
+    fn add(&mut self, uuid: Arc<Uuid>, msg: Bytes, msg_byte_size: u32) -> Result<(), String>;
+    fn del(&mut self, uuid: Arc<Uuid>) -> Result<(), String>;
+    fn fetch(&mut self) -> Result<Vec<(Arc<Uuid>, u32)>, String>;
 }
 
 #[cfg(test)]
@@ -13,13 +14,16 @@ mod tests {
 
 
     use msg_store::Uuid;
-    use std::collections::BTreeMap;
+    use std::{
+        collections::BTreeMap,
+        sync::Arc
+    };
 
     use crate::{Db, Bytes};
 
     struct MemDb {
-        msgs: BTreeMap<Uuid, Bytes>,
-        msg_data: BTreeMap<Uuid, u32>
+        msgs: BTreeMap<Arc<Uuid>, Bytes>,
+        msg_data: BTreeMap<Arc<Uuid>, u32>
     }
 
     impl MemDb {
@@ -32,24 +36,24 @@ mod tests {
     }
 
     impl Db for MemDb {
-        fn get<'a>(&mut self, uuid: Uuid) -> Result<Bytes, String> {
+        fn get(&mut self, uuid: Arc<Uuid>) -> Result<Bytes, String> {
             let msg = match self.msgs.get(&uuid){
                 Some(msg) => Ok(msg),
                 None => Err("Hello".to_string())
             }?;
             Ok(msg.clone())
         }
-        fn add(&mut self, uuid: Uuid, msg: Bytes, msg_byte_size: u32) -> Result<(), String> {
-            self.msgs.insert(uuid, msg);
+        fn add(&mut self, uuid: Arc<Uuid>, msg: Bytes, msg_byte_size: u32) -> Result<(), String> {
+            self.msgs.insert(uuid.clone(), msg);
             self.msg_data.insert(uuid, msg_byte_size);
             Ok(())
         }
-        fn del(&mut self, uuid: Uuid) -> Result<(), String> {
+        fn del(&mut self, uuid: Arc<Uuid>) -> Result<(), String> {
             self.msgs.remove(&uuid);
             self.msg_data.remove(&uuid);
             Ok(())
         }
-        fn fetch(&mut self) -> Result<Vec<(Uuid, u32)>, String> {
+        fn fetch(&mut self) -> Result<Vec<(Arc<Uuid>, u32)>, String> {
             Ok(vec![])
         }
     }
@@ -62,7 +66,7 @@ mod tests {
         let uuid = Uuid::from_string("0-0-0").unwrap();
         let inserted_msg = "hello, world".as_bytes();
 
-        db.add(uuid, Bytes::copy_from_slice(inserted_msg), 4).unwrap();
+        db.add(uuid.clone(), Bytes::copy_from_slice(inserted_msg), 4).unwrap();
         let received_msg = db.get(uuid).unwrap();
         assert_eq!(inserted_msg, received_msg);
     }
